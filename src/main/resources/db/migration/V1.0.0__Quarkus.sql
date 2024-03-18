948477ddad2035fff87078f5d679653ebb82c86d
henriquepercudeoliveira <henrique.percu@gmail.com>
2024-03-17 22:31:42 -0300

    Adding db schema

---
+++ src/main/resources/db/migration/V1.0.0__Quarkus.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS NOTE
+(
+    note_id UUID PRIMARY KEY default gen_random_uuid(),
+    title VARCHAR(1000)  NOT NULL
+    description VARCHAR  NOT NULL
+    user_id VARCHAR(1000)  NOT NULL
+
+);
+
